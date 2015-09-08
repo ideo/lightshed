b153c20104f939bfb610eaa1565b802be71ab292
Ethan Klein <ethan@niceness.org>
2015-09-07 22:56:47 -0700

    figuring our scroll magic

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,9 @@
+/* 
+IDEO for UNVR (UN Virtual Reality)
+
+*/
+
+
 var unvr = {
   setup: function() {
     this.scrollSetup();
@@ -5,14 +11,28 @@ var unvr = {
 
   scrollSetup: function() {
     var controller = new ScrollMagic.Controller();
-    var scene = new ScrollMagic.Scene({
-      offset: 600 // start scene after scrolling for 100px
-    }).addTo(controller);
 
-    scene.on("enter", function (event) {
-      $('.vid1').fadeOut(3000);
+    var scene1 = new ScrollMagic.Scene({
+      triggerElement: '.startScene1',
+      triggerHook: '0'
+    }).addTo(controller)
+      .addIndicators({name: "1 (duration: 0)"});
+
+    scene1.on("enter leave", function (event) {
+      console.log('enter leave scene 1');
+      $('.vid1').fadeIn(3000);
     });
 
+    var scene2 = new ScrollMagic.Scene({
+      triggerElement: '.startScene2',
+      triggerHook: 'onEnter'
+    }).addTo(controller)
+      .addIndicators({name: "1 (duration: 0)"});
+
+    scene2.on("enter leave", function (event) {
+      console.log('enter leave scene 2');
+      $('.vid1').fadeOut(3000);
+    });
 
   }
 }
@@ -21,3 +41,10 @@ var unvr = {
 $(function() {
   unvr.setup();
 });
+
+
+
+// Scene
+// var scene2 = new ScrollScene({triggerElement: "#screen3 .imacInner", triggerHook: 'onEnter', offset: 203})
+//     .addTo(controller)
+//     .setTween(splitAnimation);
