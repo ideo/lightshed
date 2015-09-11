49601cee9694822c52147f061bb9166129b47c73
Ethan Klein <ethan@niceness.org>
2015-09-11 12:03:51 -0700

    beginnings of nav

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,16 +10,22 @@ var unvr = {
   setup: function() {
     this.checkIfMobile();
     this.nav();
-    this.calcWidth();
+    this.resize();
     if (!this.isMobile) {
       this.horizScrollSetup();
       this.scrollFlip();
       this.animateBackground();
     }
+    this.calcWidth();
     // this.titleTextMorph();
     // this.carouselSetup();
   },
 
+  resize: function() {
+    $( window ).resize(function() {
+      unvr.calcWidth();
+    });
+  },
 
   checkIfMobile: function() {
     var isMobile = false; //initiate as false
@@ -28,19 +34,19 @@ var unvr = {
         || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) isMobile = true;
 
     this.isMobile = isMobile;
-},
+  },
 
   // determine the total width to allow for unbroken horizontal content
   calcWidth: function() {
     var totalWidth = 0;
     $('section').each(function() {
-      totalWidth = totalWidth + $(this).width();
+      totalWidth = totalWidth + $(this).outerWidth();
     });
     $('.foreground').width(totalWidth);
+    console.log(totalWidth);
   },
 
   titleTextMorph: function() {
-    console.log('morphing');
     $('.title_text_container .top').addClass('fade_me_out');
     $('.title_text_container .bottom').addClass('fade_me_in');
   },
@@ -79,7 +85,11 @@ var unvr = {
     $('.next-slide').on('click', function() {
       $(".owl-carousel").trigger('next.owl.carousel');
     });
+  },
 
+  changeNav: function(navItem) {
+    $('.nav_item').removeClass('active');
+    $(navItem).addClass('active');
   },
 
   horizScrollSetup: function() {
@@ -92,8 +102,10 @@ var unvr = {
       .addIndicators({name: "begin blur"});
     scene1.on("enter", function (event) {
       $('.horiz_background').addClass('blur_me');
+      unvr.changeNav('#nav2');
     });
     scene1.on("leave", function (event) {
+      unvr.changeNav('#nav1');
       $('.horiz_background').removeClass('blur_me');
     });
 
@@ -103,9 +115,11 @@ var unvr = {
     }).addTo(controller)
       .addIndicators({name: "end blur"});
     scene2.on("enter", function (event) {
+      unvr.changeNav('#nav3');
       $('.horiz_background').removeClass('blur_me');
     });
     scene2.on("leave", function (event) {
+      unvr.changeNav('#nav2');
       $('.horiz_background').addClass('blur_me');
     });
 
